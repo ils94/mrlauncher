7a4c992217440e10d0932e37792310068c134233
None <None>
2022-12-02 21:15:47 -0300

    A lot of changes

--- mrlauncher.py
+++ mrlauncher.py
@@ -1,10 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-from tkinter import *
-from tkinter import messagebox
-from tkinter.ttk import Progressbar
+from tkinter import Tk, Button, Text, messagebox, ttk, Menu, Frame, Label, X, E, W, END, CURRENT
 import threading
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM
 import os
 import time
 from os import system
@@ -52,28 +50,27 @@ class HyperlinkManager:
 
 
 def multithreading(function):
-    try:
-        threading.Thread(target=function).start()
-    except Exception as e:
-        errorMessage(e)
+    t = threading.Thread(target=function)
+    t.setDaemon(True)
+    t.start()
 
 
-def errorMessage(msg):
+def error_message(msg):
     label_checking["text"] = "Something went wrong! Please try again."
     start_button["state"] = "normal"
     messagebox.showerror("Error", msg)
 
 
-def repairGame():
+def repair_game():
     value = messagebox.askyesno("Repair Game",
                                 "This will re-download the game to fix file corruption. Do you wish to continue?")
 
     if value:
         label_checking["text"] = "Re-downloading the game, please wait..."
-        threading.Thread(target=downloadGame).start()
+        multithreading(download_game)
 
 
-def checkJava():
+def check_java():
     check = system("java -version") == 0
     if not check:
         value = messagebox.askokcancel("Install Java",
@@ -82,7 +79,7 @@ def checkJava():
             webbrowser.open("https://www.java.com/en/download/manual.jsp")
 
 
-def pingServer():
+def ping_server():
     try:
         server_status["text"] = "Checking server status..."
         server_status["fg"] = "black"
@@ -99,195 +96,167 @@ def pingServer():
             server_status["fg"] = "red"
             server_status["text"] = "Server OFF"
     except Exception as e:
-        errorMessage(e)
+        error_message(e)
 
 
-def bs4Soup(link):
+def bs4soup(link):
     try:
-        return BeautifulSoup(requests.get(link).text, 'html.parser')
+        referer = "https://www.google.com/"
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                                 "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/108.0.0.0 Safari/537.36", "referer": referer}
+
+        return BeautifulSoup(requests.get(link, headers=headers).text, 'html.parser')
     except Exception as e:
-        errorMessage(e)
+        error_message(e)
 
 
-def gatherInfos():
-    try:
-        global releases_article
-        global releases_link
-        global hotfix_article
-        global hotfix_link
-        global version
-        global download_link
+def gather_infos():
+    global releases_article
+    global releases_link
+    global hotfix_article
+    global hotfix_link
+    global version
+    global download_link
 
-        start_button["state"] = "disabled"
+    start_button["state"] = "disabled"
 
-        soup = bs4Soup("https://www.miragerealms.co.uk/devblog/category/releases/")
+    soup = bs4soup("https://www.miragerealms.co.uk/devblog/category/releases/")
 
-        releases = soup.findAll('article')
+    releases = soup.findAll('article')
 
-        releases_article = str(releases[0].text).replace("Read More", "").lstrip().rstrip()
+    releases_article = str(releases[0].text).replace("Read More", "").lstrip().rstrip()
 
-        releases_link = findLinks(soup, "/releases/version")
+    releases_link = find_links(soup, "/releases/version")
 
-        soup = bs4Soup("https://www.miragerealms.co.uk/devblog/category/hotfixes/")
+    soup = bs4soup("https://www.miragerealms.co.uk/devblog/category/hotfixes/")
 
-        hotfixes = soup.findAll('article')
+    hotfixes = soup.findAll('article')
 
-        hotfix_article = str(hotfixes[0].text).replace("Read More", "").lstrip().rstrip()
+    hotfix_article = str(hotfixes[0].text).replace("Read More", "").lstrip().rstrip()
 
-        hotfix_link = findLinks(soup, "/hotfixes/")
+    hotfix_link = find_links(soup, "/hotfixes/")
 
-        soup = bs4Soup("https://www.miragerealms.co.uk/devblog/play/")
+    soup = bs4soup("https://www.miragerealms.co.uk/devblog/play/")
 
-        site_version = soup.find_all('h4', attrs={'elementor-heading-title elementor-size-default'})
+    site_version = soup.find_all('h4', attrs={'elementor-heading-title elementor-size-default'})
 
-        version = site_version[0].text
+    version = site_version[0].text
 
-        download_link = findLinks(soup, ".jar")
+    download_link = find_links(soup, ".jar")
 
-        loadNews()
-        checkVersion()
-        start_button["state"] = "normal"
-
-    except Exception as e:
-        errorMessage(e)
+    load_news()
+    check_version()
+    start_button["state"] = "normal"
 
 
-def findLinks(soup, string):
-    try:
-        links1 = []
-        links2 = []
+def find_links(soup, string):
+    links1 = []
+    links2 = []
 
-        for a in soup.find_all('a', href=True):
-            if a.text:
-                links1.append(a['href'])
+    for a in soup.find_all('a', href=True):
+        if a.text:
+            links1.append(a['href'])
 
-        for link in links1:
-            if string in link:
-                links2.append(link)
+    for link in links1:
+        if string in link:
+            links2.append(link)
 
-        return links2[0]
-    except Exception as e:
-        errorMessage(e)
+    return links2[0]
 
 
-def startGame():
-    try:
-        if os.path.isfile("mr.jar") and os.path.isfile("version.txt"):
-            if is_update_to_date:
-                start_button["state"] = "disabled"
-                label_checking["text"] = "The game is launching!"
-                os.startfile("mr.jar")
-                time.sleep(3)
-                os._exit(0)
-            else:
-                label_checking["text"] = "Downloading new update, please wait..."
-                downloadGame()
+def start_game():
+    if os.path.isfile("mr.jar") and os.path.isfile("version.txt"):
+        if is_update_to_date:
+            start_button["state"] = "disabled"
+            label_checking["text"] = "The game is launching!"
+            os.startfile("mr.jar")
+            time.sleep(3)
+            os._exit(0)
         else:
-            label_checking["text"] = "Downloading the game, please wait..."
-            downloadGame()
-    except Exception as e:
-        errorMessage(e)
+            label_checking["text"] = "Downloading new update, please wait..."
+            download_game()
+    else:
+        label_checking["text"] = "Downloading the game, please wait..."
+        download_game()
 
 
-def startGameThread():
-    try:
-        threading.Thread(target=startGame).start()
-    except Exception as e:
-        errorMessage(e)
+def download_game():
+    global download_link
 
+    start_button["state"] = "disabled"
 
-def downloadGame():
-    try:
-        global download_link
+    save_version(version)
 
-        start_button["state"] = "disabled"
+    file_name = "mr.jar"
+    with open(file_name, "wb") as f:
+        response = requests.get(download_link, stream=True)
+        total_length = response.headers.get('content-length')
 
-        saveVersion(version)
-
-        file_name = "mr.jar"
-        with open(file_name, "wb") as f:
-            response = requests.get(download_link, stream=True)
-            total_length = response.headers.get('content-length')
-
-            if total_length is None:
-                f.write(response.content)
-            else:
-                dl = 0
-                total_length = int(total_length)
-                for data in response.iter_content(chunk_size=4096):
-                    dl += len(data)
-                    f.write(data)
-                    done = int(50 * dl / total_length)
-                    pb["value"] = done * 2
-
-        pb["value"] = 0
-        label_checking["text"] = "Download complete! Game is now launching!"
-        os.startfile(file_name)
-        time.sleep(3)
-        os._exit(0)
-    except Exception as e:
-        errorMessage(e)
-
-
-def saveVersion(ver):
-    try:
-        file = open("version.txt", "w")
-        file.write(ver)
-        file.close()
-    except Exception as e:
-        errorMessage(e)
-
-
-def loadVersion():
-    try:
-        file = open("version.txt")
-        ver = file.read()
-        file.close()
-        return ver
-    except Exception as e:
-        saveVersion("None")
-        print(e)
-
-
-def checkVersion():
-    try:
-        global is_update_to_date
-        if os.path.isfile("version.txt"):
-            if loadVersion() == version:
-                label_checking["text"] = "The client is up to date!"
-                is_update_to_date = True
-            else:
-                label_checking["text"] = "There is a new update available!"
+        if total_length is None:
+            f.write(response.content)
         else:
-            label_checking["text"] = "The game is ready to be downloaded!"
-    except Exception as e:
-        errorMessage(e)
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                pb["value"] = done * 2
+
+    pb["value"] = 0
+    label_checking["text"] = "Download complete! Game is now launching!"
+    os.startfile(file_name)
+    time.sleep(3)
+    os._exit(0)
 
 
-def loadNews():
-    try:
-        text_news["state"] = "normal"
-        global releases_article
-        global releases_link
-        global hotfix_article
-        global hotfix_link
+def save_version(ver):
+    file = open("version.txt", "w")
+    file.write(ver)
+    file.close()
 
-        hyperlink = HyperlinkManager(text_news)
 
-        text_news.delete("1.0", END)
+def load_version():
+    file = open("version.txt")
+    ver = file.read()
+    file.close()
+    return ver
 
-        text_news.insert("1.0", "LASTEST RELEASE VERSION:\n\n")
-        text_news.insert(END, releases_article)
-        text_news.insert(END, "\n\n" + releases_link, hyperlink.add(partial(webbrowser.open, releases_link)))
 
-        text_news.insert(END, "\n\n------------------------------------------------------------")
-        text_news.insert(END, "\n\nLASTEST HOTFIXES:\n\n")
-        text_news.insert(END, hotfix_article)
-        text_news.insert(END, "\n\n" + hotfix_link, hyperlink.add(partial(webbrowser.open, hotfix_link)))
+def check_version():
+    global is_update_to_date
+    if os.path.isfile("version.txt"):
+        if load_version() == version:
+            label_checking["text"] = "The client is up to date!"
+            is_update_to_date = True
+        else:
+            label_checking["text"] = "There is a new update available!"
+    else:
+        label_checking["text"] = "The game is ready to be downloaded!"
 
-        text_news["state"] = "disabled"
-    except Exception as e:
-        errorMessage(e)
+
+def load_news():
+    text_news["state"] = "normal"
+    global releases_article
+    global releases_link
+    global hotfix_article
+    global hotfix_link
+
+    hyperlink = HyperlinkManager(text_news)
+
+    text_news.delete("1.0", END)
+
+    text_news.insert("1.0", "LASTEST RELEASE VERSION:\n\n")
+    text_news.insert(END, releases_article)
+    text_news.insert(END, "\n\n" + releases_link, hyperlink.add(partial(webbrowser.open, releases_link)))
+
+    text_news.insert(END, "\n\n------------------------------------------------------------")
+    text_news.insert(END, "\n\nLASTEST HOTFIXES:\n\n")
+    text_news.insert(END, hotfix_article)
+    text_news.insert(END, "\n\n" + hotfix_link, hyperlink.add(partial(webbrowser.open, hotfix_link)))
+
+    text_news["state"] = "disabled"
 
 
 root = Tk()
@@ -310,9 +279,9 @@ menu_bar = Menu(root)
 
 menu = Menu(menu_bar, tearoff=0)
 
-menu.add_command(label="Repair...", command=lambda: multithreading(repairGame))
-menu.add_command(label="Update server status...", command=lambda: multithreading(pingServer))
-menu.add_command(label="Reload launcher...", command=lambda: multithreading(gatherInfos))
+menu.add_command(label="Repair Game", command=lambda: multithreading(repair_game))
+menu.add_command(label="Update server status", command=lambda: multithreading(ping_server))
+menu.add_command(label="Reload launcher", command=lambda: multithreading(gather_infos))
 
 menu_bar.add_cascade(label="Menu", menu=menu)
 
@@ -331,16 +300,16 @@ label_checking.pack(padx=5, fill=X)
 frame = Frame(root)
 frame.pack(fill=X, padx=5)
 
-start_button = Button(frame, text="Play!", width=15, height=2, command=lambda: multithreading(startGame))
+start_button = Button(frame, text="Play!", width=15, height=2, command=lambda: multithreading(start_game))
 start_button.pack(anchor=E, side="right")
 
-pb = Progressbar(frame, mode="determinate", length=100)
+pb = ttk.Progressbar(frame, mode="determinate", length=100)
 pb.pack(fill=X, padx=5, anchor=W, side="bottom")
 
-multithreading(gatherInfos)
+multithreading(gather_infos)
 
-multithreading(pingServer)
+multithreading(ping_server)
 
-multithreading(checkJava)
+multithreading(check_java)
 
 root.mainloop()
